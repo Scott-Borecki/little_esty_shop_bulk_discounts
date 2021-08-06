7a2709e9e2ae4c61d0197899fc4f68dc0e31c222
Scott Borecki <scottborecki@gmail.com>
2021-08-05 18:56:49 -0600

    Refactor Items Ready to Ship view and test

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -97,28 +97,35 @@ RSpec.describe 'merchant dashboard' do
 
   it "can see a section for Items Ready to Ship with list of names of items ordered and ids" do
     within("#items_ready_to_ship") do
-      expect(page).to have_content(@item_1.name)
-      expect(page).to have_content(@item_1.invoice_ids)
+      items = [@item_1, @item_2]
 
-      expect(page).to have_content(@item_2.name)
-      expect(page).to have_content(@item_2.invoice_ids)
+      items.each do |item|
+        expect(page).to have_content(item.name)
+        item.invoice_ids.each do |invoice_id|
+          expect(page).to have_content(invoice_id)
+        end
+      end
 
       expect(page).to have_no_content(@item_3.name)
-      expect(page).to have_no_content(@item_3.invoice_ids)
+
+      @item_3.invoice_ids.each do |invoice_id|
+        expect(page).to have_no_content(invoice_id)
+      end
+
     end
   end
 
   it "each invoice id is a link to my merchant's invoice show page " do
-    expect(page).to have_link(@item_1.invoice_ids.to_s)
-    expect(page).to have_link(@item_2.invoice_ids.to_s)
-    expect(page).to_not have_link(@item_3.invoice_ids.to_s)
+    expect(page).to have_link(@item_1.invoice_ids.first.to_s)
+    expect(page).to have_link(@item_2.invoice_ids.first.to_s)
+    expect(page).to_not have_link(@item_3.invoice_ids.first.to_s)
 
-    click_link(@item_1.invoice_ids.to_s, match: :first)
-    
+    click_link(@item_1.invoice_ids.first.to_s, match: :first)
+
     expect(current_path).to eq("/merchant/#{@merchant1.id}/invoices/#{@invoice_1.id}")
   end
 
   it "shows the date that the invoice was created in this format: Monday, July 18, 2019" do
-    expect(page).to have_content(@invoice_1.created_at.strftime("%A, %B %-d, %Y"))
+    expect(page).to have_content(@invoice_1.formatted_time)
   end
 end
