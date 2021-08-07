01aecc56176a5ea1b716574d8fa2f8ff34da6fe2
Scott Borecki <scottborecki@gmail.com>
2021-08-07 11:47:19 -0600

    Refactor tests

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -1,74 +1,74 @@
 require 'rails_helper'
 
-describe 'Admin Invoices Index Page' do
-  before :each do
-    @m1 = Merchant.create!(name: 'Merchant 1')
+describe 'admin invoice show page (/admin/invoices/:id)' do
+  # See spec/object_creation_helper.rb for objection creation details
+  admin_invoice_show_objects
 
-    @c1 = Customer.create!(first_name: 'Yo', last_name: 'Yoz', address: '123 Heyyo', city: 'Whoville', state: 'CO', zip: 12345)
-    @c2 = Customer.create!(first_name: 'Hey', last_name: 'Heyz')
+  describe 'as an admin' do
+    describe 'when I visit an admin invoice show page (/admin/invoices/:id)' do
+      before { visit admin_invoice_path(invoice1) }
 
-    @i1 = Invoice.create!(customer_id: @c1.id, status: 2, created_at: '2012-03-25 09:54:09')
-    @i2 = Invoice.create!(customer_id: @c2.id, status: 1, created_at: '2012-03-25 09:30:09')
+      it 'displays the invoice detials: id, status, and created_at' do
+        expect(current_path).to eq(admin_invoice_path(invoice1))
 
-    @item_1 = Item.create!(name: 'test', description: 'lalala', unit_price: 6, merchant_id: @m1.id)
-    @item_2 = Item.create!(name: 'rest', description: 'dont test me', unit_price: 12, merchant_id: @m1.id)
+        expect(page).to have_content("Invoice ##{invoice1.id}")
+        expect(page).to have_content("Created on:\n#{invoice1.formatted_time}")
 
-    @ii_1 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_1.id, quantity: 12, unit_price: 2, status: 0)
-    @ii_2 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_2.id, quantity: 6, unit_price: 1, status: 1)
-    @ii_3 = InvoiceItem.create!(invoice_id: @i2.id, item_id: @item_2.id, quantity: 87, unit_price: 12, status: 2)
+        expect(page).to have_no_content("Invoice ##{invoice2.id}")
+      end
 
-    visit admin_invoice_path(@i1)
-  end
+      it 'displays the customer details: name and address' do
+        expect(page).to have_content("#{customer1.first_name} #{customer1.last_name}")
+        expect(page).to have_content(customer1.address)
+        expect(page).to have_content("#{customer1.city}, #{customer1.state} #{customer1.zip}")
 
-  it 'should display the id, status and created_at' do
-    expect(page).to have_content("Invoice ##{@i1.id}")
-    expect(page).to have_content("Created on:\n#{@i1.created_at.strftime("%A, %B %d, %Y")}")
+        expect(page).to have_no_content("#{customer2.first_name} #{customer2.last_name}")
+      end
 
-    expect(page).to_not have_content("Invoice ##{@i2.id}")
-  end
+      it 'displays all the items on the invoice and the item details: name, quantity, price, and status' do
+        expect(page).to have_content(item1.name)
+        expect(page).to have_content(item2.name)
 
-  it 'should display the customers name and shipping address' do
-    expect(page).to have_content("#{@c1.first_name} #{@c1.last_name}")
-    expect(page).to have_content(@c1.address)
-    expect(page).to have_content("#{@c1.city}, #{@c1.state} #{@c1.zip}")
+        expect(page).to have_content(invoice_item1.quantity)
+        expect(page).to have_content(invoice_item2.quantity)
 
-    expect(page).to_not have_content("#{@c2.first_name} #{@c2.last_name}")
-  end
+        expect(page).to have_content(invoice_item1.unit_price)
+        expect(page).to have_content(invoice_item2.unit_price)
 
-  it 'should display all the items on the invoice' do
-    expect(page).to have_content(@item_1.name)
-    expect(page).to have_content(@item_2.name)
+        expect(page).to have_content(invoice_item1.status)
+        expect(page).to have_content(invoice_item2.status)
 
-    expect(page).to have_content(@ii_1.quantity)
-    expect(page).to have_content(@ii_2.quantity)
+        expect(page).to have_no_content(invoice_item3.quantity)
+        expect(page).to have_no_content(invoice_item3.unit_price)
+        expect(page).to have_no_content(invoice_item3.status)
+      end
 
-    expect(page).to have_content("$#{@ii_1.unit_price}")
-    expect(page).to have_content("$#{@ii_2.unit_price}")
+      it 'displays the total revenue' do
+        expect(page).to have_content("Total Revenue:\n$#{invoice1.total_revenue}")
 
-    expect(page).to have_content(@ii_1.status)
-    expect(page).to have_content(@ii_2.status)
+        expect(page).to have_no_content(invoice2.total_revenue)
+      end
 
-    expect(page).to_not have_content(@ii_3.quantity)
-    expect(page).to_not have_content("$#{@ii_3.unit_price}")
-    expect(page).to_not have_content(@ii_3.status)
-  end
+      it 'displays the total discounted' do
+        expect(page).to have_content("Total Discounted Revenue:\n$#{invoice1.total_discounted_revenue}")
 
-  it 'should display the total revenue the invoice will generate' do
-    expect(page).to have_content("Total Revenue:\n$#{@i1.total_revenue}")
+        expect(page).to have_no_content(invoice2.total_discounted_revenue)
+      end
 
-    expect(page).to_not have_content(@i2.total_revenue)
-  end
+      it 'has a status select field that updates the invoices status' do
+        within "#status-update-#{invoice1.id}" do
+          expect(invoice1.status).to eq('completed')
+          expect(page).to have_select('invoice[status]', selected: 'completed')
+          expect(page).to have_button('Update Invoice')
 
-  it 'should have status as a select field that updates the invoices status' do
-    within("#status-update-#{@i1.id}") do
-      select('cancelled', :from => 'invoice[status]')
+          select 'cancelled', from: :invoice_status
+          click_button 'Update Invoice'
 
-      expect(page).to have_button('Update Invoice')
-
-      click_button 'Update Invoice'
-
-      expect(current_path).to eq(admin_invoice_path(@i1))
-      expect(@i1.status).to eq('completed')
+          expect(current_path).to eq(admin_invoice_path(invoice1))
+          expect(page).to have_select('invoice[status]', selected: 'cancelled')
+          expect(invoice1.reload.status).to eq('cancelled')
+        end
+      end
     end
   end
 end
