de0887cec58fac0cd168f18e2ccc3ea3852e3cbc
Scott Borecki <scottborecki@gmail.com>
2021-08-08 12:09:11 -0600

    Update to single-quoted strings

--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'merchant invoices show (/merchants/:merchant_id/invoices/:invoic
     describe 'when I visit my merchant invoices show page' do
       before { visit merchant_invoice_path(merchant3, invoice3) }
 
-      it "displays the invoice id" do
+      it 'displays the invoice id' do
         other_invoices = Invoice.all.to_a
         other_invoices.delete(invoice3)
         expect(other_invoices).to_not include(invoice3)
@@ -22,7 +22,7 @@ RSpec.describe 'merchant invoices show (/merchants/:merchant_id/invoices/:invoic
         end
       end
 
-      it "displays the invoice and customer details" do
+      it 'displays the invoice and customer details' do
         other_invoices = Invoice.all.to_a
         other_invoices.delete(invoice3)
         expect(other_invoices).to_not include(invoice3)
@@ -39,7 +39,7 @@ RSpec.describe 'merchant invoices show (/merchants/:merchant_id/invoices/:invoic
         within('#invoice-status') { expect(page).to have_content(invoice3.status.humanize) }
       end
 
-      it "displays the total revenue for this invoice" do
+      it 'displays the total revenue for this invoice' do
         expect(page).to have_content("Total Revenue: $#{invoice3.total_revenue}")
       end
 
@@ -47,7 +47,7 @@ RSpec.describe 'merchant invoices show (/merchants/:merchant_id/invoices/:invoic
         expect(page).to have_content("Total Discounted Revenue: $#{invoice3.total_discounted_revenue}")
       end
 
-      it "displays the details of the invoice items" do
+      it 'displays the details of the invoice items' do
         invoice3.invoice_items.each do |invoice_item|
           within "#ii-#{invoice_item.id}" do
             expect(page).to have_content(invoice_item.item.name)
@@ -57,28 +57,28 @@ RSpec.describe 'merchant invoices show (/merchants/:merchant_id/invoices/:invoic
         end
       end
 
-      it "displays a select field to update the invoice status" do
+      it 'displays a select field to update the invoice status' do
         within '#invoice-status' do
-          expect(page).to have_content("Completed")
+          expect(page).to have_content('Completed')
 
-          expect(page).to have_no_content("In progress")
-          expect(page).to have_no_content("in progress")
-          expect(page).to have_no_content("Cancelled")
-          expect(page).to have_no_content("cancelled")
+          expect(page).to have_no_content('In progress')
+          expect(page).to have_no_content('in progress')
+          expect(page).to have_no_content('Cancelled')
+          expect(page).to have_no_content('cancelled')
         end
 
         within "#ii-#{invoice_item3a.id}" do
-          page.select "cancelled"
-          click_button "Update Invoice"
+          page.select 'cancelled'
+          click_button 'Update Invoice'
         end
 
         within '#invoice-status' do
-          expect(page).to have_content("Cancelled")
+          expect(page).to have_content('Cancelled')
 
-          expect(page).to have_no_content("In progress")
-          expect(page).to have_no_content("in progress")
-          expect(page).to have_no_content("Completed")
-          expect(page).to have_no_content("completed")
+          expect(page).to have_no_content('In progress')
+          expect(page).to have_no_content('in progress')
+          expect(page).to have_no_content('Completed')
+          expect(page).to have_no_content('completed')
         end
       end
 
