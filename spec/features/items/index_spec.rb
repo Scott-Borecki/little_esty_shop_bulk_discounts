577058d37bb4affe77f14592da9ec478d47a6058
Scott Borecki <scottborecki@gmail.com>
2021-08-08 12:11:45 -0600

    Update to single-quoted strings

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -5,15 +5,15 @@ describe "merchant items index" do
     @merchant1 = Merchant.create!(name: 'Hair Care')
     @merchant2 = Merchant.create!(name: 'Jewelry')
 
-    @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
-    @item_2 = Item.create!(name: "Conditioner", description: "This makes your hair shiny", unit_price: 8, merchant_id: @merchant1.id)
-    @item_3 = Item.create!(name: "Brush", description: "This takes out tangles", unit_price: 5, merchant_id: @merchant1.id)
-    @item_4 = Item.create!(name: "Hair tie", description: "This holds up your hair", unit_price: 1, merchant_id: @merchant1.id)
-    @item_7 = Item.create!(name: "Scrunchie", description: "This holds up your hair but is bigger", unit_price: 3, merchant_id: @merchant1.id)
-    @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
+    @item_1 = Item.create!(name: 'Shampoo', description: 'This washes your hair', unit_price: 10, merchant_id: @merchant1.id, status: 1)
+    @item_2 = Item.create!(name: 'Conditioner', description: 'This makes your hair shiny', unit_price: 8, merchant_id: @merchant1.id)
+    @item_3 = Item.create!(name: 'Brush', description: 'This takes out tangles', unit_price: 5, merchant_id: @merchant1.id)
+    @item_4 = Item.create!(name: 'Hair tie', description: 'This holds up your hair', unit_price: 1, merchant_id: @merchant1.id)
+    @item_7 = Item.create!(name: 'Scrunchie', description: 'This holds up your hair but is bigger', unit_price: 3, merchant_id: @merchant1.id)
+    @item_8 = Item.create!(name: 'Butterfly Clip', description: 'This holds up your hair but in a clip', unit_price: 5, merchant_id: @merchant1.id)
 
-    @item_5 = Item.create!(name: "Bracelet", description: "Wrist bling", unit_price: 200, merchant_id: @merchant2.id)
-    @item_6 = Item.create!(name: "Necklace", description: "Neck bling", unit_price: 300, merchant_id: @merchant2.id)
+    @item_5 = Item.create!(name: 'Bracelet', description: 'Wrist bling', unit_price: 200, merchant_id: @merchant2.id)
+    @item_6 = Item.create!(name: 'Necklace', description: 'Neck bling', unit_price: 300, merchant_id: @merchant2.id)
 
     @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
     @customer_2 = Customer.create!(first_name: 'Cecilia', last_name: 'Jones')
@@ -22,8 +22,8 @@ describe "merchant items index" do
     @customer_5 = Customer.create!(first_name: 'Sylvester', last_name: 'Nader')
     @customer_6 = Customer.create!(first_name: 'Herber', last_name: 'Coon')
 
-    @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
-    @invoice_2 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-28 14:54:09")
+    @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: '2012-03-27 14:54:09')
+    @invoice_2 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: '2012-03-28 14:54:09')
     @invoice_3 = Invoice.create!(customer_id: @customer_2.id, status: 2)
     @invoice_4 = Invoice.create!(customer_id: @customer_3.id, status: 2)
     @invoice_5 = Invoice.create!(customer_id: @customer_4.id, status: 2)
@@ -50,7 +50,7 @@ describe "merchant items index" do
     visit merchant_items_path(@merchant1)
   end
 
-  it "can see a list of all the names of my items and not items for other merchants" do
+  it 'can see a list of all the names of my items and not items for other merchants' do
     expect(page).to have_content(@item_1.name)
     expect(page).to have_content(@item_2.name)
     expect(page).to have_content(@item_3.name)
@@ -66,71 +66,71 @@ describe "merchant items index" do
     expect(page).to have_link(@item_3.name)
     expect(page).to have_link(@item_4.name)
 
-    within "#enabled-items" do
+    within '#enabled-items' do
       click_link "#{@item_1.name}"
 
       expect(current_path).to eq("/merchant/#{@merchant1.id}/items/#{@item_1.id}")
     end
   end
 
-  it "can make a button to disable items" do
+  it 'can make a button to disable items' do
     within "#item-#{@item_1.id}" do
       click_button "Disable"
 
       item = Item.find(@item_1.id)
-      expect(item.status).to eq("disabled")
+      expect(item.status).to eq('disabled')
     end
 
     within "#item-#{@item_2.id}" do
-      click_button "Enable"
+      click_button 'Enable'
 
       item = Item.find(@item_2.id)
-      expect(item.status).to eq("enabled")
+      expect(item.status).to eq('enabled')
     end
 
     within "#item-#{@item_3.id}" do
-      click_button "Enable"
+      click_button 'Enable'
 
       item = Item.find(@item_3.id)
-      expect(item.status).to eq("enabled")
+      expect(item.status).to eq('enabled')
     end
   end
 
-  it "has a section for disabled items" do
-    within "#disabled-items" do
+  it 'has a section for disabled items' do
+    within '#disabled-items' do
       expect(page).to have_content(@item_3.name)
       expect(page).to have_content(@item_2.name)
       expect(page).to_not have_content(@item_1.name)
     end
   end
 
-  it "has a section for enabled items" do
-    within "#enabled-items" do
+  it 'has a section for enabled items' do
+    within '#enabled-items' do
       expect(page).to_not have_content(@item_2.name)
       expect(page).to_not have_content(@item_3.name)
       expect(page).to have_content(@item_1.name)
     end
   end
 
-  it "has a link to create a new item" do
-    click_link "Create New Item"
+  it 'has a link to create a new item' do
+    click_link 'Create New Item'
 
     expect(current_path).to eq(new_merchant_item_path(@merchant1))
 
-    fill_in "Name", with: "Bar Shampoo"
-    fill_in "Description", with: "Eco friendly shampoo"
-    fill_in "Unit price", with: "15"
-    click_button "Submit"
+    fill_in 'Name', with: 'Bar Shampoo'
+    fill_in 'Description', with: 'Eco friendly shampoo'
+    fill_in 'Unit price', with: '15'
+    click_button 'Submit'
 
     expect(current_path).to eq(merchant_items_path(@merchant1))
 
-    within "#disabled-items" do
-      expect(page).to have_content("Bar Shampoo")
+    within '#disabled-items' do
+      expect(page).to have_content('Bar Shampoo')
     end
   end
 
-  it "shows the top 5 most popular items by total revenue" do
-    within "#top-items" do
+  it 'shows the top 5 most popular items by total revenue' do
+    within '#top-items' do
       expect(@item_1.name).to appear_before(@item_2.name)
       expect(@item_2.name).to appear_before(@item_3.name)
       expect(@item_3.name).to appear_before(@item_8.name)
@@ -140,8 +140,8 @@ describe "merchant items index" do
     end
   end
 
-  it "links the top 5 to the item show page" do
-    within "#top-items" do
+  it 'links the top 5 to the item show page' do
+    within '#top-items' do
       expect(page).to have_link(@item_1.name)
       expect(page).to have_link(@item_2.name)
       expect(page).to have_link(@item_3.name)
@@ -154,8 +154,8 @@ describe "merchant items index" do
     end
   end
 
-  it "shows the total revenue next to the item" do
-    within "#top-items" do
+  it 'shows the total revenue next to the item' do
+    within '#top-items' do
       expect(page).to have_content("#{@merchant1.top_5_items[0].total_revenue}")
       expect(page).to have_content("#{@merchant1.top_5_items[1].total_revenue}")
       expect(page).to have_content("#{@merchant1.top_5_items[2].total_revenue}")
@@ -164,8 +164,8 @@ describe "merchant items index" do
     end
   end
 
-  it "shows the best day next to the item" do
-    within "#top-items" do
+  it 'shows the best day next to the item' do
+    within '#top-items' do
       expect(page).to have_content("Top selling date for #{@item_1.name} was #{@item_1.best_day}")
       expect(page).to have_content("Top selling date for #{@item_2.name} was #{@item_2.best_day}")
       expect(page).to have_content("Top selling date for #{@item_3.name} was #{@item_3.best_day}")
