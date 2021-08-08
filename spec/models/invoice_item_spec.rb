bb4d7ed2fed91b9db972b205e91231f64564cbb5
Scott Borecki <scottborecki@gmail.com>
2021-08-08 11:49:45 -0600

    Add #revenue

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -8,9 +8,19 @@ RSpec.describe InvoiceItem, type: :model do
     it { should validate_presence_of :unit_price }
     it { should validate_presence_of :status }
   end
-  
+
   describe "relationships" do
     it { should belong_to :invoice }
     it { should belong_to :item }
+  end
+
+  describe 'instance methods' do
+    describe '#revenue' do
+      it 'returns the revenue' do
+        invoice_item = create(:invoice_item, :shipped, quantity: 4, unit_price: 10)
+
+        expect(invoice_item.revenue).to eq(40)
+      end
+    end
   end
 end
