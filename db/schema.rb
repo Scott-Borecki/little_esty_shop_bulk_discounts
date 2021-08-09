825a320a8cc9b0b2d091bf56bc828da0af6c03e6
Scott Borecki <scottborecki@gmail.com>
2021-08-09 14:26:34 -0600

    Change data type to string for CC number and exp date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_215857) do
+ActiveRecord::Schema.define(version: 2021_08_09_202224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,8 +74,8 @@ ActiveRecord::Schema.define(version: 2021_08_06_215857) do
   end
 
   create_table "transactions", force: :cascade do |t|
-    t.bigint "credit_card_number"
-    t.bigint "credit_card_expiration_date"
+    t.string "credit_card_number"
+    t.string "credit_card_expiration_date"
     t.integer "result"
     t.bigint "invoice_id"
     t.datetime "created_at", null: false
