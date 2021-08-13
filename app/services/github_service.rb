be27d0752dfef1161e99895d74886ef1f8ed2dd8
Scott Borecki <scottborecki@gmail.com>
2021-08-13 17:40:37 -0600

    Refactor services

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,6 +1,8 @@
 class GithubService
   def self.repository
-    response = Faraday.get('https://api.github.com/repos/Scott-Borecki/little_esty_shop_bulk_discounts')
-    JSON.parse(response.body, symbolize_names: true)
+    url      = 'https://api.github.com/repos/Scott-Borecki/little_esty_shop_bulk_discounts'
+    response = Faraday.get(url).body
+
+    JSON.parse(response, symbolize_names: true)
   end
 end
--- app/services/holiday_service.rb
+++ app/services/holiday_service.rb
@@ -1,6 +1,8 @@
 class HolidayService
   def self.holidays(country_code = 'US')
-    response = Faraday.get("https://date.nager.at/api/v3/NextPublicHolidays/#{country_code}")
+    url      = "https://date.nager.at/api/v3/NextPublicHolidays/#{country_code}"
+    response = Faraday.get(url)
+
     JSON.parse(response.body, symbolize_names: true)
   end
 end
