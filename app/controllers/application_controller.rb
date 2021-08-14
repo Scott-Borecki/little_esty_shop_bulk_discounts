035480f0c6d9c480e6129de7b697282a0b4072d7
Scott Borecki <scottborecki@gmail.com>
2021-08-13 20:47:40 -0600

    Move GitHub info to only welcome page to reduce API requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,7 @@
 class ApplicationController < ActionController::Base
-  before_action :fetch_repository
-
   private
 
   def error_message(errors)
     errors.full_messages.join(', ')
-  end
-
-  def fetch_repository
-    @repository = GithubFacade.repository
   end
 end
