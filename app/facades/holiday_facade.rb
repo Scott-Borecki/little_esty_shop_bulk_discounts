169fa6599ec5057dc0ed64a60528c61b86e3974f
Scott Borecki <scottborecki@gmail.com>
2021-08-14 14:02:39 -0600

    Remove whitespace

--- app/facades/holiday_facade.rb
+++ app/facades/holiday_facade.rb
@@ -1,7 +1,6 @@
 class HolidayFacade
   def self.upcoming_holidays(quantity = 3)
     holidays = HolidayService.holidays
-    
     holidays[0..(quantity - 1)].map do |holiday_data|
       Holiday.new(holiday_data)
     end
