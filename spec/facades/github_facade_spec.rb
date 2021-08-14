34ff411b8d276dcdceb00f0c9a78c3afca30446a
Scott Borecki <scottborecki@gmail.com>
2021-08-14 14:18:56 -0600

    Add underscores as thousands separators

--- spec/facades/github_facade_spec.rb
+++ spec/facades/github_facade_spec.rb
@@ -10,14 +10,14 @@ RSpec.describe GithubFacade, type: :facade do
         expect(repository.repo_name).to eq('little_esty_shop_bulk_discounts')
         expect(repository.url).to eq('https://github.com/Scott-Borecki/little_esty_shop_bulk_discounts')
         expect(repository.owner).to eq('Scott-Borecki')
-        expect(repository.owner_id).to eq(79381792)
+        expect(repository.owner_id).to eq(79_381_792)
         expect(repository.owner_url).to eq('https://github.com/Scott-Borecki')
       end
     end
 
     describe '.number_of_commits' do
       it 'returns the number of commits' do
-        user_id = 79381792
+        user_id = 79_381_792
         commits = GithubFacade.number_of_commits(user_id)
 
         expect(commits).to be_an(Integer)
@@ -26,7 +26,7 @@ RSpec.describe GithubFacade, type: :facade do
 
     describe '.number_of_pull_requests' do
       it 'returns the number of pull requests' do
-        user_id = 79381792
+        user_id = 79_381_792
         number_of_pull_requests = GithubFacade.number_of_pull_requests(user_id)
 
         expect(number_of_pull_requests).to be_an(Integer)
